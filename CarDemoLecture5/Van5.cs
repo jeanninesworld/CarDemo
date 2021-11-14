94ea9304d4370365bf0d78ec8aa72b9234b27c92
Jeannine.Kwasnik <jeanninesworld@hotmail.com>
2021-11-14 15:44:51 -0500

    Seperated Lecture 4 from 5 hands on

---
+++ CarDemoLecture5/Van5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDemo5
+{
+    public class Van5 : Vehicle5
+    {
+        public Van5(string make, string model, string color, string year, string type) : base(make, model, color, year, type){}
+
+        public void Start()
+        {
+            invokeKeylessStart();
+        }
+        public void invokeKeylessStart()
+        {
+            Console.WriteLine($"Started{Type} automatically  with key fob");
+        }
+    }
+}
